7476bc4627e1b5151d9d6426670e2d81f00764bd
dipinbajra <bajradipin98@gmail.com>
2022-06-02 17:46:09 +0545

    validation using PHP in add_data.php

--- add_data.php
+++ add_data.php
@@ -4,6 +4,29 @@ if( isset($_POST['add_data'])) {
     $phone=$_POST['phone'];
     $email=$_POST['email'];
 
+    if($name == NULL || $name == "" ) {
+        echo "name is required";
+        return;
+    }
+
+   
+        
+        
+
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo(" Please enter  valid email address");
+        return false;
+      
+      }  
+      
+    else{
+       
+       
+     
+
+    
+
     echo "The Name is: ". $name ." <br>" ."The Contact No. is: ". $phone. "<br>"."The E-mail Address is:". $email;
 
     include 'db.php';
@@ -14,7 +37,11 @@ if( isset($_POST['add_data'])) {
     if($result){
         header('Location:index.php');
     }
+    return true;
+}
 } else {
     die('s');
 }
 ?>
+
+
