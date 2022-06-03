6598c225664cae01bedb180a9fa5fad62380f7a5
dipinbajra <bajradipin98@gmail.com>
2022-06-03 16:52:51 +0545

    used get method to pull the data and display message of validation on the main page

--- add_data.php
+++ add_data.php
@@ -1,44 +1,40 @@
 <?php
-if( isset($_POST['add_data'])) {
+
+if( isset( $_POST['add_data'] ) ) {
     $name = $_POST['name'];
     $phone=$_POST['phone'];
     $email=$_POST['email'];
-
-    if($name == NULL || $name == "" ) {
-        echo "name is required";
-        return;
-    }
-
-   
-        
-        
-
-
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo(" Please enter  valid email address");
-        return false;
-      
-      }  
-      
-    else{
-       
-       
-     
-
     
+    $email_is_valid = 'valid';
+    $name_is_valid = 'valid';  
 
-    echo "The Name is: ". $name ." <br>" ."The Contact No. is: ". $phone. "<br>"."The E-mail Address is:". $email;
-
-    include 'db.php';
-
-    $sql="INSERT INTO contact_table(name, phone, email) VALUES('$name' , '$phone', '$email')";
-    $result=mysqli_query($conn, $sql);
-
-    if($result){
-        header('Location:index.php');
+    if( $name == NULL || $name == "" ) {
+        $name_is_valid = "invalid";
     }
-    return true;
-}
+
+    if ( ! filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $email_is_valid = "invalid";
+    }
+
+
+    if( $email_is_valid == 'invalid' || $name_is_valid == 'invalid' ) {
+        header('Location:index.php?email_status=' . $email_is_valid .'&name_status=' . $name_is_valid );
+    } else {
+        echo "The Name is: ". $name ." <br>" ."The Contact No. is: ". $phone. "<br>"."The E-mail Address is:". $email;
+
+        include 'db.php';
+
+        $sql="INSERT INTO contact_table(name, phone, email) VALUES('$name' , '$phone', '$email')";
+        $result=mysqli_query($conn, $sql);
+
+        if( $result ) {
+            header('Location:index.php' );
+        }
+    }
+    
+    if ($result){
+        header('Location:index.php?submission_status='.$result);
+    }
 } else {
     die('s');
 }
--- index.php
+++ index.php
@@ -6,17 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/validate.js/0.13.1/validate.min.js"></script>
     <script src="js/main.js"></script>  
   
     <title>Contact Book</title>
 </head>
-<body onload="document.form1.email.focus()" >
+<body  >
     <header>
         <h1 class>Contact List</h1>
     </header>
-    <section class="first-section ">
+
+    <div class="message-invalid">
+        <?php 
+            if( isset( $_GET['email_status'] ) && $_GET['email_status'] == 'invalid') {
+                echo "Email is invalid!";
+            }
+        ?> 
+    </div>
+
+     <section class="first-section ">
         <div class="container ">
-        <Table >
+            <Table >
             <tr>
                 <th>Name</th>
                 <th>Contact No.</th>
@@ -25,40 +35,55 @@
             </tr>
             <?php
 
-            include 'db.php';
+                include 'db.php';
 
-            $sql="SELECT * FROM contact_table";
-            $result=mysqli_query($conn, $sql);
+                $sql="SELECT * FROM contact_table";
+                $result=mysqli_query($conn, $sql);
 
-            if($result){
-                while($row=mysqli_fetch_assoc($result)){
+                if($result){
+                 while($row=mysqli_fetch_assoc($result)){
                     $id=$row['id'];
                     $name=$row['name'];
                     $phone=$row['phone'];
                     $email=$row['email'];
-                    ?>
-                <tr>
-                    <td><?php echo $name ?></td>
-                    <td><?php echo $phone ?></td>
-                    <td><?php echo $email ?></td>
+            ?>
+                    <tr>
+                        <td><?php echo $name ?></td>
+                        <td><?php echo $phone ?></td>
+                        <td><?php echo $email ?></td>
 
-                    <td><a href="delete.php?id=<?php echo $id?>">Delete</a></td>
+                        <td><a href="delete.php?id=<?php echo $id?>">Delete</a></td>
             
-                </tr>
-            <?php
-                }
-            }
+                     </tr>
+             <?php
+                        }
+                    }
 
             ?>
-
+    
            
-        </Table>
-    </div>
+            </Table>
+        </div>
     </section>
     <section class="second-section ">
-        <form action="add_data.php " method="POST" name="form1" onsubmit="return validateEmail(document.form1.email)">
+        <form action="add_data.php " method="POST" name="form1" >
+
+            <div class="message-invalid">         
+                <?php 
+                    if( isset( $_GET['name_status'] ) && $_GET['name_status']== 'invalid' ){
+                        echo "Please enter name";
+                    }
+                ?>
+            </div>  
+            <div class="submission-success">
+                <?php
+                    if( isset( $_GET['submission_status'] ) && $_GET[ 'submission_status']==true )
+                        echo "Submission Successful !"
+                ?>
+            </div> 
             <label for="Name">Name:</label>
             <input type="text " name="name" id="name" class="mb-3" >
+            
             <label for="Conatact No.">Contact No.:</label>
             <input type="number " name="phone" id="phone" class="mb-3" required>
             <label for="E-mail">E-mail:</label>
